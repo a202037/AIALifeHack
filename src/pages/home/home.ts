e010f27abc8539aa6894907fb06f86182e37bc79
Christopher Chu <christopherchu96@gmail.com>
2018-07-25 21:52:17 +0800

    Initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,38 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import {Camera} from 'ionic-native';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+ public base64Image: string;
   constructor(public navCtrl: NavController) {
     
   }
+ takePicture(){
+    Camera.getPicture({
+        destinationType: Camera.DestinationType.DATA_URL,
+        targetWidth: 1000,
+        targetHeight: 1000,
+        quality: 100
+    }).then((imageData) => {
+        this.base64Image = "data:image/jpeg;base64," + imageData;
+    }, (err) => {
+        console.log(err);
+    });
+  }
 
+  accessGallery(){
+   Camera.getPicture({
+     sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+     destinationType: Camera.DestinationType.DATA_URL
+    }).then((imageData) => {
+      this.base64Image = 'data:image/jpeg;base64,'+imageData;
+     }, (err) => {
+      console.log(err);
+    });
+  }
 }
